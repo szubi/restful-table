de366284dc9ce1c9f2be1c56378910b7f94648be
Wojciech <w.szubi@gmail.com>
2020-01-17 00:26:12 +0100

    Update EditRow.js

--- src/components/edit/EditRow.js
+++ src/components/edit/EditRow.js
@@ -4,42 +4,42 @@ import EditCell from './EditCell';
 import { DataContext } from '../../context/DataContext';
 
 function EditRow({ row }) {
-        const { headers, handleUpdateRowSubmit, handleUpdateCancel } = useContext(DataContext);
+    const { headers, handleUpdateRowSubmit, handleUpdateCancel } = useContext(DataContext);
 
-        return (
-                <tr>
-                        {Object.keys(row).map((column, i) => (
-                                headers.map(header => (
+    return (
+        <tr>
+            {Object.keys(row).map((column, i) => (
+                headers.map(header => (
 
-                                        header.id === column &&
-                                        <React.Fragment key={column}>
-                                                {column !== 'id' && header.allowEdit ?
-                                                        <EditCell
-                                                                rowId={row.id}
-                                                                column={column}
-                                                                value={row[column]}
-                                                                type={header.type}
-                                                        />
-                                                        :
-                                                        <td>{row[column]}</td>
-                                                }
-
-                                                {(i + 1) === Object.keys(row).length &&
-                                                        <td>
-                                                                <button onClick={handleUpdateRowSubmit} type="submit">Update</button>
-                                                                <button onClick={handleUpdateCancel}>Cancel</button>
-                                                        </td>
-                                                }
-                                        </React.Fragment>
-                                ))
-                        ))
+                    header.id === column &&
+                    <React.Fragment key={column}>
+                        {column !== 'id' && header.allowEdit ?
+                            <EditCell
+                                rowId={row.id}
+                                column={column}
+                                value={row[column]}
+                                type={header.type}
+                            />
+                            :
+                            <td>{row[column]}</td>
                         }
-                </tr>
-        );
+
+                        {(i + 1) === Object.keys(row).length &&
+                            <td>
+                                <button onClick={handleUpdateRowSubmit} type="submit">Update</button>
+                                <button onClick={handleUpdateCancel}>Cancel</button>
+                            </td>
+                        }
+                    </React.Fragment>
+                ))
+            ))
+            }
+        </tr>
+    );
 };
 
 EditRow.propTypes = {
-        row: PropTypes.object,
+    row: PropTypes.object,
 };
 
 export default EditRow;
